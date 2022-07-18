ebbdbec852b7ff9b22725fe152e119f462c698a7
Israel Andrade DEV <israeltandrade@hotmail.com>
2022-07-18 20:35:37 -0300

    3 examples of functions

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -1,3 +1,14 @@
+// Função com fat arrow:
 const add = (a: number, b: number): number => {
   return a + b;
 };
+
+// Função com nome:
+function divide(a: number, b: number):number {
+  return a / b;
+}
+
+// Função anônima atribuída à uma constante:
+const multiply = function(a: number, b: number):number {
+  return a * b;
+}
