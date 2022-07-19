6e98096a549e49a004ca072981f357c2d91baf7d
Israel Andrade DEV <israeltandrade@hotmail.com>
2022-07-18 21:09:11 -0300

    void and never

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -1,14 +1,29 @@
-// Função com fat arrow:
 const add = (a: number, b: number): number => {
   return a + b;
 };
 
-// Função com nome:
-function divide(a: number, b: number):number {
+function divide(a: number, b: number): number {
   return a / b;
-}
+};
 
-// Função anônima atribuída à uma constante:
-const multiply = function(a: number, b: number):number {
+const multiply = function(a: number, b: number): number {
   return a * b;
-}
+};
+
+const logger = (message: string): void => {
+  console.log(message);
+};
+
+// Usamos never quando temos certeza de nunca retornar nada
+// com a função.
+const throwError = (message: string): never => {
+  throw new Error(message);
+};
+
+// Uso mais comum:
+const throwError = (message: string): string => {
+  if (!message) {
+    throw new Error(message);
+  }
+  return message;
+};
