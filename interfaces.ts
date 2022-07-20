fbab1d26f2e28466eece5dafb8dbed942abecc47
Israel Andrade DEV <israeltandrade@hotmail.com>
2022-07-19 23:54:22 -0300

    complex types and functions on interfaces

--- interfaces.ts
+++ interfaces.ts
@@ -1,23 +1,26 @@
-// Uma interface é um novo tipo, com primeira letra maiúscula,
-// e se utiliza um termo generalizador.
+// Podemos passar tipos complexos e funções em uma interface:
 interface Vehicle {
   name: string;
-  year: number;
+  year: Date;
   broken: boolean;
+  // Agora a interface obriga a colocação de uma função com
+  // retorno string nos objetos à ela associados:
+  summary(): string;
 }
 
+// Objeto ajustado para comportar a interface:
 const oldCivic = {
   name: 'civic',
-  year: 2000,
-  broken: true
+  year: new Date(),
+  broken: true,
+  summary(): string {
+    return `Name: ${this.name}`;
+  }
 };
 
-// Ao invés de passar a tipagem de cada parâmetro, se passa
-// um objeto com a interface Vehicle:
+
 const printVehicle = (vehicle: Vehicle): void => {
-  console.log(`Name: ${vehicle.name}`);
-  console.log(`Year: ${vehicle.year}`);
-  console.log(`Broken? ${vehicle.broken}`);
+  console.log(vehicle.summary());
 };
 
 // O objeto "oldCivic" é passado como parâmetro da função:
