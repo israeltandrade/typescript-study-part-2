bfd9632421f9742fd405fc2d2ab6f6f05ca0ce85
Israel Andrade DEV <israeltandrade@hotmail.com>
2022-07-20 21:06:23 -0300

    fields in classes

--- classes.ts
+++ classes.ts
@@ -1,21 +1,39 @@
 class Vehicle {
-  // Método protegido só pode ser acessado pela classe e classes herdeiras:
+  // Campos ou propriedades (sem construtor, é bom inicializar):
+  // color: string = 'red';
+  color: string;
+
+  constructor(color: string) {
+    this.color = color;
+  }
+
+  // A forma a seguir dispensa a definição da propriedade no começo do arquivo:
+  // constructor(public color: string) {}
+
   protected honk(): void {
     console.log('bee beeeep!')
   }
 }
 
+const vehicle = new Vehicle('orange');
+console.log(vehicle.color);
+
 class Car extends Vehicle {
   // Método privado:
   private drive(): void {
-    console.log('vuruuuuuuuum!')
+    console.log('vuruuuuuuuum!');
+  }
+
+  private showColor(): void {
+    console.log(this.color);
   }
 
   public startDrivingProcess(): void {
     this.drive();
     this.honk();
+    this.showColor();
   }
 }
 
-const car = new Car();
+const car = new Car('azul');
 car.startDrivingProcess();
